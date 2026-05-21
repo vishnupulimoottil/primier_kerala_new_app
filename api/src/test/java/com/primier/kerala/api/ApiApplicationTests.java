9e88b1c6c56c6f36e47e1b79200d153a3651365b
Vishnu P S <vishnuvishnushaji5@gmail.com>
2026-05-21 14:23:35 +0530

    start from scrach: just app created

---
+++ api/src/test/java/com/primier/kerala/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primier.kerala.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
